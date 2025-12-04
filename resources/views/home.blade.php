7821520caa0e6a9dde78e6ca0d9b337af033c32b
egor-diw <egor.alexandrovich@dealinweb.com>
2025-12-04 13:10:58 +0200

    home commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,31 +5,46 @@
 
 @section('content')
     <div class="flex gap-1.5 text-white">
-        @auth
-            <p>Welcome, authenticated user!</p>
-            <button type="button"
-                    class="text-white bg-gradient-to-br from-purple-600 to-blue-500
-                    hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300
-                     dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5
-                      text-center leading-5">
-                <a href="{{route('logout')}}">
-                    Log out
-                </a>
-            </button>
-        @endauth
+        <div class="container mx-auto flex items-center justify-between p-2">
 
-        @guest
+            @auth
+                <p>Welcome, authenticated user!</p>
                 <button type="button"
                         class="text-white bg-gradient-to-br from-purple-600 to-blue-500
                     hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300
                      dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5
                       text-center leading-5">
-                    <a href="{{route('login.show')}}">
-                        Log in
+                    <a href="{{route('logout')}}">
+                        Log out
                     </a>
                 </button>
-            <p>Please log in or register.</p>
-        @endguest
+            @endauth
+
+            @guest
+                <div class="flex gap-3">
+                    <button type="button"
+                            class="text-white bg-gradient-to-br from-purple-600 to-blue-500
+                    hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300
+                     dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5
+                      text-center leading-5">
+                        <a href="{{route('login.show')}}">
+                            Log in
+                        </a>
+                    </button>
+                    <button type="button"
+                            class="text-white bg-gradient-to-br from-purple-600 to-blue-500
+                    hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300
+                     dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5
+                      text-center leading-5">
+                        <a href="{{route('register.show')}}">
+                            Register
+                        </a>
+                    </button>
+                </div>
+
+                <p>Please log in or register.</p>
+            @endguest
+        </div>
     </div>
 
 @endsection
